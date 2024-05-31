af8381b56728776c578494505b138f5a83201505
kalpesh <chudasamakalpesh41@gmail.com>
2024-05-31 12:28:27 +0530

    i create urls for my html file weather html file for

--- weatherapp/urls.py
+++ weatherapp/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from weather.views import weathpage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('weather/',weathpage,name="weathepage")
 ]
